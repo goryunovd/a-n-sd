a2d3ffca578e6376fa844281c1fffbfb76c1167e
Горюнов Дмитрий <dsgoryunov@stud.etu.ru>
2021-11-07 13:14:10 +0300

    some wrong shit

--- laba1a'n'sd/Nodeclass.h
+++ laba1a'n'sd/Nodeclass.h
@@ -9,17 +9,19 @@ class Node
 {
 public:
 	Node(int initdata = 0, Node*next = NULL, Node*prev = NULL): data(data), next(next), prev(prev) {}
-	~Node()=default;
-	int getData() { return this->data; }
-	int getNextData() { if (this->next == NULL)  return 0; else return this->next->data; }
-	int getPrevData() { if (this->prev == NULL)  return 0; else return this->prev->data; }
-	void setData(int newData) { this->data= newData; }
-	void setNext(Node* newnext) {this->next = newnext;}
-	void setPrev(Node* newprev) { this->prev = newprev;}
+	//Node(int initdata, Node*next, Node*prev) :data(data), next(next), prev(prev) {}
 
-	Node* getNext()  { if (this->next == NULL) { return 0; } else return this->next; }
-	Node* getPrev()  { if (this->prev== NULL) { return 0; } else return this->prev; }
+	int getData() { return data; }
+	int getNextData() { if (next == NULL)  return 0; else return next->data; }
+	int getPrevData() { if (prev == NULL)  return 0; else return prev->data; }
+	void setData(int newData) { data= newData; }
+	void setNext(Node* newnext) {next = newnext;}
+	void setPrev(Node* newprev) {prev = newprev;}
+
+	Node* getNext()  { if (next == NULL) { return 0; } else return next; }
+	Node* getPrev()  { if (prev== NULL) { return 0; } else return prev; }
 	Node* makeNull() { return this->next = NULL; }
+	~Node()=default;
 private:
 	int data;
 	Node* next;Node* prev;
